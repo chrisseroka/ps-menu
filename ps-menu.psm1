0da9eab089e8f9182045a63f0a9ccefff7ed03c3
Krzysztof Seroka <krzysztof.seroka@softwareone.com>
2017-02-06 13:55:29 +0100

    adding multiselection

--- ps-menu.psm1
+++ ps-menu.psm1
@@ -1,38 +1,62 @@
 function DrawMenu {
-    param ($menuItems, $menuPosition)
+    param ($menuItems, $menuPosition, $Multiselect, $selection)
     $l = $menuItems.length
     for ($i = 0; $i -le $l;$i++) {
 		if ($menuItems[$i] -ne $null){
+			$item = $menuItems[$i]
+			if ($Multiselect)
+			{
+				if ($selection -contains $i){
+					$item = '[x] ' + $item
+				}
+				else {
+					$item = '[ ] ' + $item
+				}
+			}
 			if ($i -eq $menuPosition) {
-				Write-Host "> $($menuItems[$i])" -ForegroundColor Green
+				Write-Host "> $($item)" -ForegroundColor Green
 			} else {
-				Write-Host "  $($menuItems[$i])"
+				Write-Host "  $($item)"
 			}
 		}
     }
 }
 
+function Toggle-Selection {
+	param ($pos, [array]$selection)
+	if ($selection -contains $pos){ 
+		$result = $selection | where {$_ -ne $pos}
+	}
+	else {
+		$selection += $pos
+		$result = $selection
+	}
+	$result
+}
+
 function Menu {
-    param ([array]$menuItems, [switch]$ReturnIndex=$false)
+    param ([array]$menuItems, [switch]$ReturnIndex=$false, [switch]$Multiselect)
     $vkeycode = 0
     $pos = 0
+    $selection = @()
     $cur_pos = [System.Console]::CursorTop
     [console]::CursorVisible=$false #prevents cursor flickering
     if ($menuItems.Length -gt 0)
 	{
-		DrawMenu $menuItems $pos
+		DrawMenu $menuItems $pos $Multiselect $selection
 		While ($vkeycode -ne 13 -and $vkeycode -ne 27) {
 			$press = $host.ui.rawui.readkey("NoEcho,IncludeKeyDown")
 			$vkeycode = $press.virtualkeycode
 			If ($vkeycode -eq 38 -or $press.Character -eq 'k') {$pos--}
 			If ($vkeycode -eq 40 -or $press.Character -eq 'j') {$pos++}
+			If ($press.Character -eq ' ') { $selection = Toggle-Selection $pos $selection }
 			if ($pos -lt 0) {$pos = 0}
 			If ($vkeycode -eq 27) {$pos = $null }
 			if ($pos -ge $menuItems.length) {$pos = $menuItems.length -1}
 			if ($vkeycode -ne 27)
 			{
 				[System.Console]::SetCursorPosition(0,$cur_pos)
-				DrawMenu $menuItems $pos
+				DrawMenu $menuItems $pos $Multiselect $selection
 			}
 		}
 	}
@@ -42,16 +66,22 @@ function Menu {
 	}
     [console]::CursorVisible=$true
 
-    if ($pos -eq $null)
+    if ($ReturnIndex -eq $false -and $pos -ne $null)
 	{
-		return $null
-	}
-    elseif ($ReturnIndex -eq $false)
-	{
-		return $menuItems[$pos]
+		if ($Multiselect){
+			return $menuItems[$selection]
+		}
+		else {
+			return $menuItems[$pos]
+		}
 	}
 	else 
 	{
-		return $pos
+		if ($Multiselect){
+			return $selection
+		}
+		else {
+			return $pos
+		}
 	}
 }
